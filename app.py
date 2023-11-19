78d82665d865a831500d91f54543802cc6d18fe8
Simon Schulze <simon.schulze@s2021.tu-chemnitz.de>
2023-11-19 17:24:20 +0100

    Requirements changed

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def establish_connection() -> sql.Connection:
     return con
 
 
-@st.cache_resource()
+@st.cache_resource(experimental_allow_widgets=True)
 def get_manager() -> stx.CookieManager:
 
     """
