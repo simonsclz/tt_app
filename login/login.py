b1ae7fc8451972f13ff47f04381446af6e7022ba
Simon Schulze <simon.schulze@s2021.tu-chemnitz.de>
2023-11-19 19:27:17 +0100

    Cookie management changed again

--- login/login.py
+++ login/login.py
@@ -11,20 +11,6 @@ import sqlite3 as sql
 # Description: This is the script that handles the login process.
 
 
-def set_cookie(cm: stx.CookieManager) -> None:
-
-    """
-    Sets the cookie on clicking the button.
-    :param cm: The STX cookie manager.
-    :return: None.
-    """
-
-    # set the log-in-cookie to keep users logged in for 10 minutes
-
-    expires_at = datetime.datetime.now() + datetime.timedelta(0, 600)
-    cm.set(key="log_in", cookie="logged_in", val=True, expires_at=expires_at)
-
-
 def login(form_ph, warning_ph, con: sql.Connection, cm: stx.CookieManager) -> (bool, str):
 
     """
@@ -39,7 +25,7 @@ def login(form_ph, warning_ph, con: sql.Connection, cm: stx.CookieManager) -> (b
         with st.form(key="login_form"):
             user_name = st.text_input("Benutzername:")
             password = st.text_input("Passwort:", type="password")
-            clicked = st.form_submit_button("Anmelden!", on_click=set_cookie(cm))
+            clicked = st.form_submit_button("Anmelden!", on_click=st.experimental_rerun())
 
     if clicked:
         password = sha512(password.encode('utf-8'))
@@ -51,7 +37,11 @@ def login(form_ph, warning_ph, con: sql.Connection, cm: stx.CookieManager) -> (b
 
         if len(result) == 1:  # check the password hashes
             if result[0][0] == password.hexdigest():
+
+                # set the log-in-cookie to keep users logged in for 10 minutes
+
                 expires_at = datetime.datetime.now() + datetime.timedelta(0, 600)
+                cm.set(key="log_in", cookie="logged_in", val=True, expires_at=expires_at)
                 cm.set(key="user_name", cookie="user_name", val=user_name, expires_at=expires_at)
                 return True, user_name
             else:
