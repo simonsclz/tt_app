0f2660df896b48789f3be0efd347d8a194ae923d
Simon Schulze <simon.schulze@s2021.tu-chemnitz.de>
2023-11-19 17:25:05 +0100

    Debug

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ def app() -> None:
     if not logged_in and cm.get("logged_in") is None:  # also test the log-in-cookie
         st.stop()
 
+    print(user_name)
     form_ph.empty()
     display(data_ph, con, cm.get("user_name"))  # gets executed only if logged in
 
